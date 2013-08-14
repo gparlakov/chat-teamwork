d39f8159d831cfa5f3a3c35dc04021d320c3c343
gparlakov <gparlakov@gmail.com>
2013-08-14 08:08:57 -0700

    Added MessagesModel and used it in returning all messages

---
+++ WS-Team-Work/Chat.Services/Models/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chat.Services.Models
+{
+    public class MessageModel
+    {
+        public string Content { get; set; }
+
+        public int Id { get; set; }
+    }
+}
