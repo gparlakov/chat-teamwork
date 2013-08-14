d5c9ade63f843995c41cce5c99d8350d4d5eaf38
gparlakov <gparlakov@gmail.com>
2013-08-14 08:28:42 -0700

    Add (wrong) MessageModel

---
+++ WS-Team-Work/Chat.Repositories/MessageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chat.Repositories
+{
+    public class MessageModel
+    {
+        public int Id { get; set; }
+
+        public string Content { get; set; }
+    }
+}
