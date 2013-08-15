c08504ffebb600836a31a0cc68f433498cfe88e1
gparlakov <gparlakov@gmail.com>
2013-08-15 02:14:14 -0700

    Really added NotificationMode

---
+++ WS-Team-Work/Chat.Services/Models/NotificationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace Chat.Services.Models
+{
+    public class NotificationModel
+    {
+        // sessionKey
+        [JsonProperty("toUser")]
+        public string ToUser { get; set; }
+
+        // nickname
+        [JsonProperty("fromUser")]
+        public string FromUser { get; set; }
+    }
+}
