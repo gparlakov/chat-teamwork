22b4ca3a4c2a93adc6f3c16a80ec2fffbccc86b6
gparlakov <gparlakov@gmail.com>
2013-08-15 03:43:19 -0700

    Added Chat link and Start UI - Login and freeze

---
+++ WS-Team-Work/Chat.Services/Scripts/controller.js
@@ -0,0 +1,124 @@
+﻿/// <reference path="class.js" />
+/// <reference path="persister.js" />
+/// <reference path="jquery-2.0.2.js" />
+/// <reference path="ui.js" />
+
+var controllers = (function () {
+
+    var updateTimer = null;
+
+    var rootUrl = "http://localhost:17097/api/";
+    var Controller = Class.create({
+        init: function () {
+            this.persister = persisters.get(rootUrl);
+        },
+        loadUI: function (selector) {
+            if (this.persister.isUserLoggedIn()) {
+                this.loadChatUI(selector);
+            }
+            else {
+                this.loadLoginFormUI(selector);
+            }
+            this.attachUIEventHandlers(selector);
+        },
+        loadLoginFormUI: function (selector) {
+            var loginFormHtml = ui.loginForm()
+            $(selector).html(loginFormHtml);
+        },
+        loadChatUI: function (selector) {
+            var self = this;
+            var gameUIHtml =
+				ui.gameUI(this.persister.nickname());
+            $(selector).html(gameUIHtml);
+
+            this.updateUI(selector);
+
+            updateTimer = setInterval(function () {
+                self.updateUI(selector);
+            }, 15000);
+        },
+        loadGame: function (selector, gameId) {
+            this.persister.game.state(gameId, function (gameState) {
+                var gameHtml = ui.gameState(gameState);
+                $(selector + " #game-holder").html(gameHtml)
+            });
+        },
+        attachUIEventHandlers: function (selector) {
+            var wrapper = $(selector);
+            var self = this;
+
+            wrapper.on("click", "#btn-show-login", function () {
+                wrapper.find(".button.selected").removeClass("selected");
+                $(this).addClass("selected");
+                wrapper.find("#login-form").show();
+                wrapper.find("#register-form").hide();
+            });
+            wrapper.on("click", "#btn-show-register", function () {
+                wrapper.find(".button.selected").removeClass("selected");
+                $(this).addClass("selected");
+                wrapper.find("#register-form").show();
+                wrapper.find("#login-form").hide();
+            });
+
+            wrapper.on("click", "#btn-login", function () {
+                var user = {
+                    username: $(selector + " #tb-login-username").val(),
+                    password: $(selector + " #tb-login-password").val()
+                }
+
+                self.persister.user.login(user, function () {
+                    self.loadChatUI(selector);
+                }, function (err) {
+                    wrapper.find("#error-messages").text("Bla");
+                });
+                return false;
+            });
+            wrapper.on("click", "#btn-register", function () {
+                var user = {
+                    username: $(selector).find("#tb-register-username").val(),
+                    nickname: $(selector).find("#tb-register-nickname").val(),
+                    password: $(selector).find("#tb-register-password").val()
+                }
+                self.persister.user.register(user, function () {
+                    self.loadChatUI(selector);
+                }, function (err) {
+                    wrapper.find("#error-messages").text("Bla");
+                });
+                return false;
+            });
+            wrapper.on("click", "#btn-logout", function () {
+                self.persister.user.logout(function () {
+                    self.loadLoginFormUI(selector);
+                    clearInterval(updateTimer);
+                }, function (err) {
+                });
+            });
+        },
+        updateUI: function (selector) {
+            this.persister.game.open(function (games) {
+                var list = ui.userList(games);
+                $(selector + " #open-games")
+					.html(list);
+            });
+            this.persister.game.myActive(function (games) {
+                var list = ui.activeGamesList(games);
+                $(selector + " #active-games")
+					.html(list);
+            });
+            //this.persister.message.all(function (msg) {
+            //    var msgList = ui.messagesList(msg);
+            //    $(selector + " #messages-holder").html(msgList);
+            //});
+        }
+    });
+    return {
+        get: function () {
+            return new Controller();
+        }
+    }
+}());
+
+$(function () {
+    var controller = controllers.get();
+    controller.loadUI("#content");
+});
---
+++ WS-Team-Work/Chat.Services/Scripts/persister.js
@@ -0,0 +1,99 @@
+﻿/// <reference path="http-requester.js" />
+/// <reference path="class.js" />
+/// <reference path="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha1.js" />
+var persisters = (function () {
+	var nickname = localStorage.getItem("nickname");
+	var sessionKey = localStorage.getItem("sessionKey");
+	var avatar = localStorage.getItem("avatar");
+	function saveUserData(userData) {
+		localStorage.setItem("nickname", userData.nickname);
+		localStorage.setItem("sessionKey", userData.sessionKey);
+        localStorage.setItem("avatar", userData.avatar)
+		nickname = userData.nickname;
+		sessionKey = userData.sessionKey;
+		avatar = userData.avatar;
+	}
+	function clearUserData() {
+		localStorage.removeItem("nickname");
+		localStorage.removeItem("sessionKey");
+		nickname = "";
+		sessionKey = "";
+	}
+
+	var MainPersister = Class.create({
+		init: function (rootUrl) {
+			this.rootUrl = rootUrl;
+			this.user = new UserPersister(this.rootUrl);
+			//this.game = new GamePersister(this.rootUrl);
+			this.message = new MessagesPersister(this.rootUrl);
+		},
+		isUserLoggedIn: function () {
+			var isLoggedIn = nickname != null && sessionKey != null;
+			return isLoggedIn;
+		},
+		nickname: function () {
+			return nickname;
+		}
+	});
+	var UserPersister = Class.create({
+		init: function (rootUrl) {
+			//...api/user/
+			this.rootUrl = rootUrl + "users/";
+		},
+		login: function (user, success, error) {
+			var url = this.rootUrl + "login";
+			var userData = {
+				username: user.username,
+				password: CryptoJS.SHA1(user.username + user.password).toString()
+			};
+
+			httpRequester.postJSON(url, userData,
+				function (data) {
+					saveUserData(data);
+					success(data);
+				}, error);
+		},
+		register: function (user, success, error) {
+			var url = this.rootUrl + "register";
+			var userData = {
+				username: user.username,
+				nickname: user.nickname,
+				password: CryptoJS.SHA1(user.username + user.password).toString()
+			};
+			httpRequester.postJSON(url, userData,
+				function (data) {
+					saveUserData(data);
+					success(data);
+				}, error);
+		},
+		logout: function (success, error) {
+			var url = this.rootUrl + "logout/" + sessionKey;
+			httpRequester.getJSON(url, function (data) {
+				clearUserData();
+				success(data);
+			}, error)
+		},
+		getAll: function (success, error) {
+		    var url = this.rootUrl + sessionKey;
+		    httpRequester.getJSON(url, success, error)
+		}
+	});
+	var MessagesPersister = Class.create({
+		init: function (url) {
+			this.rootUrl = url + "messages/";
+		},
+		unread: function (success, error) {
+			var url = this.rootUrl + "unread/" + sessionKey;
+			httpRequester.getJSON(url, success, error);
+		},
+		all: function (success, error) {
+			var url = this.rootUrl + "all/" + sessionKey;
+			httpRequester.getJSON(url, success, error);
+		}
+	});
+	return {
+		get: function (url) {
+			return new MainPersister(url);
+		}
+	};
+}());
